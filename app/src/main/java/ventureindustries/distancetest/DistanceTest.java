a5f9e0c499c7fe9f721c076941f4f55c7565f2a6
Neal Friedman <nealiof1000@gmail.com>
2016-03-14 20:30:57 -0600

    initial commit

---
+++ app/src/main/java/ventureindustries/distancetest/DistanceTest.java
@@ -0,0 +1,13 @@
+package ventureindustries.distancetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DistanceTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distance_test);
+    }
+}
