9d9dd2b7190fc2569d68120ea71051bc3470ff95
Neal Friedman <nealiof1000@gmail.com>
2016-03-14 21:22:25 -0600

    Built a basic layout

--- app/src/main/java/ventureindustries/distancetest/DistanceTest.java
+++ app/src/main/java/ventureindustries/distancetest/DistanceTest.java
@@ -2,8 +2,15 @@ package ventureindustries.distancetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class DistanceTest extends AppCompatActivity {
+
+    private TextView latitudeView;
+    private TextView longtitudeView;
+    private Button startButton;
+    private Button stopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
